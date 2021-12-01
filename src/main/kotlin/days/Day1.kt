093809ca65aa0bb47e4253eaea3d40670f52a9ab
miro <minev.miro@gmail.com>
2021-12-01 08:44:18 +0200

    day 1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,13 +3,31 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2).joinToString(" ") { it.uppercase() }
+        var result = 0
+        inputList.forEachIndexed { index, s ->
+            if (index != 0 && s.toInt() > inputList[index-1].toInt()) {
+                result++
+            }
+        }
+        return result.toString()
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        var result = 0
+        var sum = 0
+        var sumPrev = 0
+        inputList.forEachIndexed { index, s ->
+            if (index <= 2) {
+                sum += s.toInt()
+                sumPrev = sum
+            } else {
+                sum = sum + s.toInt() - inputList[index-3].toInt()
+                if (sum > sumPrev) {
+                    result++
+                }
+            }
+            sumPrev = sum
+        }
+        return result.toString()
     }
 }
